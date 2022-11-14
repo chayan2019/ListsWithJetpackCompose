c3fd7ed276b82afb7de74e48c4b1487125cf3862
Chayan <chayan@Chayans-MacBook-Pro.local>
2022-11-14 16:10:24 +0530

    Column Implementation in Jetpack Compose

--- app/src/main/java/com/withjetpack/listswithjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/withjetpack/listswithjetpackcompose/MainActivity.kt
@@ -3,13 +3,22 @@ package com.withjetpack.listswithjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.withjetpack.listswithjetpackcompose.ui.theme.ListsWithJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,6 +32,22 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
 
+                    //Coloum scrolling
+
+                    val scrollstate = rememberScrollState()
+                    Column(modifier = Modifier.verticalScroll(scrollstate)) {
+                        for (i in 1..30) {
+                            Text(
+                                text = "List Data Index $i",
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold,
+                                textAlign = TextAlign.Start,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(10.dp)
+                            )
+                        }
+                    }
                 }
             }
         }
